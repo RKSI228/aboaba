4b9196d47522a7ca02b4f89b16a17b72b5b6be8a
RKSI228 <112824290+RKSI228@users.noreply.github.com>
2022-10-22 13:18:54 +0300

    da

---
+++ тз3/dsaadads/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dsaadads
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.White, 3f);
+
+            Point[] points = new Point[1000];
+
+            for(int i=0; i<points.Length; i++)
+            {
+                points[i] = new Point(i,(int)(Math.Sin((double)i/10)*100+200));
+                graphics.DrawLine(pen, points);
+            }
+        }
+    }
+}
